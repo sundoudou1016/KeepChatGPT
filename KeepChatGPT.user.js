b4c09549a5295b201895e865788b9982cd2c3a26
未知可行 <14187349+xcanwin@users.noreply.github.com>
2026-05-04 23:34:50 +0800

    修复隐藏侧边栏时部分功能未加载问题，谢谢 crazyn2 反馈的解决方案 https://github.com/xcanwin/KeepChatGPT/issues/528

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       这是一款提高ChatGPT的数据安全能力和效率的插件。并且免费共享大量创新功能，如：自动刷新、保持活跃、数据安全、取消审计、克隆对话、言无不尽、净化页面、展示大屏、拦截跟踪、日新月异、明察秋毫等。让我们的AI体验无比安全、顺畅、丝滑、高效、简洁。
-// @version           34.6
+// @version           34.7
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -106,7 +106,7 @@
     };
 
     const u = `/api/${GM_info.script.namespace.slice(33, 34)}uth/s${GM_info.script.namespace.slice(28, 29)}ssion`;
-    const symbol1_selector = "nav.flex:not(#stage-sidebar-tiny-bar):not([inert]):not(:where([inert] *))";
+    const symbol1_selector = "nav.flex:not(#stage-sidebar-tiny-bar)";
     const symbol2_selector =
         "div.sticky div.justify-center.top-0 button span.sr-only";
     const trackingHostRegex =
